c7b44528d3f3df9b388d6709e36533fd085a95ad
Abhinav-03y <abhi745yadav@gmail.com>
2026-08-25 23:48:20 +0530

    Time: 1 ms (82.00%) | Memory: 45.1 MB (66.75%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+     int n = nums.length;
+     int [] arr = new int[101];
+
+
+
+     for(int i=0; i<n ;i++){
+        arr[nums[i]-1] = nums[i]; 
+
+
+     }
+     int i=0;
+     for(i=k-1;i<101; i=i+k){
+        if(arr[i] == 0){
+            return i+1;
+        }
+     }
+     return i+1;
+    }
+}
