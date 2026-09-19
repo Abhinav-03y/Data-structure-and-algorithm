a2ef8425fe70445a1f4746943be6c4d2c3170e92
Abhinav-03y <abhi745yadav@gmail.com>
2026-09-19 10:12:16 +0530

    Time: 4 ms (89.52%) | Memory: 46.3 MB (44.22%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,33 +1,45 @@
 class Solution {
-    int [][] dp;
-    int fun(int [] arr ,int i,int prev){
-        if(i>= arr.length){
-        return 0;
-        }
-         if (dp[i][prev + 1] != -1) {
-            return dp[i][prev + 1];
-         }
-         int max =0;
-
-        if( prev == -1 || arr[i] > arr[prev]){
-            int a = 1 + fun( arr,i+1 ,i);
-
-            max =Math.max(max, a);
-        }
-            int b = fun(arr,i+1 , prev);
-
-            max = Math.max(max, b);
-        
-        return dp[i][prev +1] = max;
-    }
     public int lengthOfLIS(int[] nums) {
-          int n = nums.length;
-        dp = new int[n][n + 1];
 
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        return fun(nums , 0 ,-1);
         
+         int[][] v1 = new int[nums.length][2];
+
+        int size = 0;
+        int i = 0;
+
+        for (int a : nums) {
+            if (size == 0 || a > v1[size - 1][0]) {
+
+
+                v1[size][0] = a;
+
+                v1[size][1] = i;
+                size++;
+            } 
+            else {
+
+                int j = 0;
+
+                while (j < size) {
+
+                    if (v1[j][0] >= a) {
+
+                        if (i > v1[j][1]) {
+
+                            v1[j][0] = a;
+                            v1[j][1] = i;
+                        }
+
+                        break;
+                    }
+
+                    j++;
+                }
+            }
+
+            i++;
+        }
+
+        return size;
     }
 }
