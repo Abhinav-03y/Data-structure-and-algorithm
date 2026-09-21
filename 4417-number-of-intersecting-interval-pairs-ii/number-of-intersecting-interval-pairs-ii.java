90b83f79b374065736cc9b129fef035a579b0d67
Abhinav-03y <abhi745yadav@gmail.com>
2026-09-21 21:58:20 +0530

    Time: 57 ms (100.00%) | Memory: 242.8 MB (100.00%) - LeetSync

--- 4417-number-of-intersecting-interval-pairs-ii/number-of-intersecting-interval-pairs-ii.java
+++ 4417-number-of-intersecting-interval-pairs-ii/number-of-intersecting-interval-pairs-ii.java
@@ -1,29 +1,35 @@
 class Solution {
+    long fun(int[] start, int[] end, int i, int j) {
+
+        if (i>=start.length)
+            return 0;
+
+        if (j >=i) {
+            return (i - j) +fun(start, end, i + 1, j);
+        }
+
+        if (end[j]< start[i]) {
+            return fun(start,end, i, j+ 1);
+        }
+
+        return (i - j) +fun(start,end, i+ 1,j);
+    }
+
     public long countIntersectingIntervals(int[][] intervals) {
 
-        int n =intervals.length;
-         int [] start = new int [n];
-        int [] end = new int [n];
+        int n=intervals.length;
 
-        for(int i=0 ; i<n ; i++){
-            start[i] = intervals[i][0];
-            end[i] = intervals[i][1];
-            
-            
+        int[] start= new int[n];
+        int[] end=new int[n];
+
+        for (int i =0;i< n;i++) {
+            start[i]= intervals[i][0];
+            end[i]= intervals[i][1];
         }
+
         Arrays.sort(start);
         Arrays.sort(end);
 
-        long ans =0;
-        int j =0;
-
-        for(int i=0;i<n ; i++ ){
-            while(j<i && end[j] <start[i]){
-              j++;                                                  
-            }
-            ans += i-j;
-            
-        }
-        return ans;
+        return fun(start, end, 0, 0);
     }
 }
